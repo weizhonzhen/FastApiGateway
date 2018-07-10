defc3e08a5ffb089c67ec21e2fb3acde731a136a
czdswzz@126.com <Weizz@404>
2018-07-10 15:23:07 +0800

    update
    update

---
+++ ApiResponse.cs
@@ -0,0 +1,56 @@
+﻿using Api.Gateway.Model;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Api.Gateway
+{
+    public class ApiResponse : IResponse
+    {
+        public void Content(HttpContext context)
+        {
+            context.Response.ContentType = "application/Json";
+            var list = BaseConfig.GetListValue<UrlModel>("ApiGateway");
+            var key = context.Request.Path.Value.Replace("/", "");
+            var content = new StreamReader(context.Request.Body).ReadToEnd();
+            var param = context.Request.QueryString.Value;
+
+            if (!list.Exists(a => a.Key.ToLower() == key.ToLower()))
+            {
+                context.Response.StatusCode = 404;
+                context.Response.WriteAsync(string.Format("请求地址无效", key), System.Text.Encoding.UTF8);
+            }
+            else
+            {
+                var item = list.Find(a => a.Key.ToLower() == key.ToLower());
+                item.Param = param;
+
+                if (item.Method.ToLower() == "post")
+                {
+                    if (item.IsContent)
+                    {
+                        item.Param = content;
+                        var info = BaseUrl.PostContent(item);
+                        context.Response.StatusCode = info.status;
+                        context.Response.WriteAsync(info.msg, System.Text.Encoding.UTF8);
+                    }
+                    else
+                    {
+                        var info = BaseUrl.PostUrl(item);
+                        context.Response.StatusCode = info.status;
+                        context.Response.WriteAsync(info.msg, System.Text.Encoding.UTF8);
+                    }
+                }
+                else if (item.Method.ToLower() == "get")
+                {
+
+                    var info = BaseUrl.GetUrl(item);
+                    context.Response.StatusCode = info.status;
+                    context.Response.WriteAsync(info.msg, System.Text.Encoding.UTF8);
+                }
+            }
+        }       
+    }
+}
---
+++ Model/UrlModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Gateway.Model
+{
+    public class UrlModel
+    {
+        /// <summary>
+        /// key
+        /// </summary>
+        public string Key { get; set; }
+
+        //down url
+        public string Url { get; set; }
+
+        /// <summary>
+        /// method
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
+        /// is body action
+        /// </summary>
+        public bool IsContent { get; set; }
+
+        /// <summary>
+        /// param
+        /// </summary>
+        public string Param { get; set; }
+
+        /// <summary>
+        /// time out
+        /// </summary>
+        public int Seconds { get; set; }
+    }
+}
