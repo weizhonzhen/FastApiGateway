99bc29c7e7b776bc9a976df820e3f05273487b2c
weizhonzhen <czdswzz@126.com>
2018-12-31 21:27:52 +0800

    Add files via upload

---
+++ FastApiGateway/FastApiGatewayHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace FastApiGateway
+{
+    public class FastApiGatewayHandler
+    {
+        public FastApiGatewayHandler(RequestDelegate request) { }
+
+        public Task InvokeAsync(HttpContext context, IFastApiGateway response)
+        {
+            response.Content(context);
+            return Task.CompletedTask;
+        }
+    }
+}
+ 
---
+++ FastApiGateway/IFastApiGateway.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FastApiGateway
+{
+    public interface IFastApiGateway
+    {
+        void Content(HttpContext content);
+    }
+}
