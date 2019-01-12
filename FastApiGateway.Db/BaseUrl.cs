c02be7bc473f948bcc97f983c00ee9329ca48595
czdswzz@126.com <czdswzz@126.com>
2019-01-12 10:38:12 +0800

    update
    update

--- FastApiGateway.Db/BaseUrl.cs
+++ FastApiGateway.Db/BaseUrl.cs
@@ -102,15 +102,52 @@ namespace FastApiGatewayDb
         }
         #endregion
 
+
+        #region post soap
+        /// <summary>
+        /// post content(insert)
+        /// </summary>
+        private static string PostSoap(string url, string method, Dictionary<string, object> param)
+        {
+            var xml = new StringBuilder();
+            xml.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            xml.Append("<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">");
+            xml.Append("<soap:Body>");
+            xml.AppendFormat("<{0} xmlns=\"http://openmas.chinamobile.com/pulgin\">", method);
+
+            foreach (KeyValuePair<string, object> item in param)
+            {
+                xml.AppendFormat("<{0}>{1}</{0}>", item.Key, item.Value);
+            }
+
+            xml.AppendFormat("</{0}>", method);
+            xml.Append("</soap:Body>");
+            xml.Append("</soap:Envelope>");
+
+            var content = new StringContent(xml.ToString(), Encoding.UTF8, "text/xml");
+            var response = http.PostAsync(new Uri(url), content).Result;
+            response.EnsureSuccessStatusCode();
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            result = result.Replace("soap:Envelope", "Envelope");
+            result = result.Replace("soap:Body", "Body");
+            result = result.Replace(" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"", "");
+            result = result.Replace(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"", "");
+            result = result.Replace(" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"", "");
+            result = result.Replace(" xmlns=\"http://openmas.chinamobile.com/pulgin\"", "");
+            return BaseXml.GetXmlString(result, string.Format("Envelope/Body/{0}Response/{0}Result", method)).Replace("&lt;", "<").Replace("&gt;", ">");
+        }
+        #endregion
+
         #region Soap url
         /// <summary>
         /// Soap url
         /// </summary>
         public static ReturnModel SoapUrl(string soapUrl,string soapParamName, string soapMethod, string soapParam)
         {
-            var hash = new Hashtable();
             var model = new ReturnModel();
-            var param = new Dictionary<string,object>();
+            var dic = new Dictionary<string,object>();
+            var param = new Dictionary<string, object>();
             try
             {
                 if (soapParam.Substring(0, 1) == "?")
@@ -121,26 +158,26 @@ namespace FastApiGatewayDb
                     foreach (var temp in soapParam.Split('&'))
                     {
                         if (temp.IndexOf('=') > 0)
-                            param.Add(temp.Split('=')[0], temp.Split('=')[1]);
+                            dic.Add(temp.Split('=')[0], temp.Split('=')[1]);
                     }
                 }
                 else
                 {
                     if (soapParam.IndexOf('=') > 0)
-                        param.Add(soapParam.Split('=')[0], soapParam.Split('=')[1]);
+                        dic.Add(soapParam.Split('=')[0], soapParam.Split('=')[1]);
                 }
 
                 if (soapParamName.IndexOf('|') > 0)
                 {
                     foreach (var temp in soapParamName.Split('|'))
                     {
-                        hash.Add(temp, param.GetValue(temp));
+                        param.Add(temp, dic.GetValue(temp));
                     }
                 }
                 else
-                    hash.Add(soapParamName, param.GetValue(soapParamName));
+                    param.Add(soapParamName, dic.GetValue(soapParamName));
 
-                model.msg = BaseWebServiceSoap.QuerySoapWebServiceString(soapUrl, soapMethod, hash, timeOut);
+                model.msg = PostSoap(soapUrl, soapMethod, param);
                 
                 model.status = 200;
                 return model;
