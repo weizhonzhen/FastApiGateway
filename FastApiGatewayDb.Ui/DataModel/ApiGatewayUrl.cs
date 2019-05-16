08ff87dc8980741395a8b92c592eaa2de4185ec7
weizhonzhen <czdswzz@126.com>
2019-05-16 10:54:11 +0800

    Add files via upload

---
+++ FastApiGatewayDb.Ui/DataModel/ApiGatewayUrl.cs
@@ -0,0 +1,59 @@
+﻿using FastData.Core.Property;
+
+namespace FastApiGatewayDb.DataModel
+{
+    /// <summary>
+    /// 接口
+    /// </summary>
+    [Table(Comments = "接口")]
+    public class ApiGatewayUrl
+    {
+        /// <summary>
+        /// key
+        /// </summary>
+        [Column(Comments = "接口key", DataType = "varchar2", Length = 16, IsNull = false)]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 接口名称
+        /// </summary>
+        [Column(Comments = "接口key", DataType = "varchar2", Length = 128, IsNull = true)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Composite(合并请求),Polling(轮循请求)
+        /// </summary>
+        [Column(Comments = "Composite(合并请求),Polling(轮循请求)", DataType = "varchar2", Length = 16, IsNull = true)]
+        public string Schema { get; set; }
+
+        /// <summary>
+        /// 是否缓存 1=是 0=否
+        /// </summary>
+        [Column(Comments = "是否缓存 1=是 0=否", DataType = "number(1,0)",IsNull =true)]
+        public int IsCache { get; set; }
+
+        /// <summary>
+        /// 缓存过期时间（天）
+        /// </summary>
+        [Column(Comments = "缓存过期时间（天）", DataType = "number(2,0)",IsNull =true)]
+        public int CacheTimeOut { get; set; }
+        
+        /// <summary>
+        /// 是否匿名访问  1=是 0=否
+        /// </summary>
+        [Column(Comments = "是否匿名访问 1=是 0=否", DataType = "number(1,0)")]
+        public int IsAnonymous { get; set; }
+
+        /// <summary>
+        /// 是否获取token
+        /// </summary>
+        [Column(Comments = "是否获取token 1=是 0=否", DataType = "number(1,0)")]
+        public int IsGetToken { get; set; }
+
+        /// <summary>
+        /// 是否数据日记
+        /// </summary>
+        [Column(Comments = "是否数据日记 1=是 0=否", DataType = "number(1,0)")]
+        public int IsLog { get; set; }
+    }
+}
---
+++ FastApiGatewayDb.Ui/DataModel/ApiGatewayUser.cs
@@ -0,0 +1,48 @@
+﻿using FastData.Core.Property;
+using System;
+
+namespace FastApiGatewayDb.DataModel
+{
+    /// <summary>
+    /// 用户信息
+    /// </summary>
+    [Table(Comments = "用户信息")]
+    public class ApiGatewayUser
+    {
+        /// <summary>
+        /// key
+        /// </summary>
+        [Column(Comments = "key", DataType = "varchar2", Length = 32, IsKey =true)]
+        public string AppKey { get; set; }
+
+        /// <summary>
+        /// 密钥
+        /// </summary>
+        [Column(Comments = "密钥", DataType = "varchar2", Length = 32, IsNull =false)]
+        public string AppSecret { get; set; }
+
+        /// <summary>
+        /// token
+        /// </summary>
+        [Column(Comments = "token", DataType = "varchar2", Length = 32, IsNull = true)]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// token过期时间 
+        /// </summary>
+        [Column(Comments = "token过期时间", DataType = "date", IsNull = true)]
+        public DateTime AccessExpires { get; set; }
+
+        /// <summary>
+        /// token ip 
+        /// </summary>
+        [Column(Comments = "token ip", DataType = "varchar2",Length =32, IsNull = true)]
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// 访问key权限逗号隔开
+        /// </summary>
+        [Column(Comments = "访问key权限逗号隔开", DataType = "varchar2", Length = 1024, IsNull = true)]
+        public string Power { get; set; }
+    }
+}
