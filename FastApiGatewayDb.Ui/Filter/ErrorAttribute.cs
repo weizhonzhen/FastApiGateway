bc624dce6e1578fd3246af2956d494baa3033654
weizhonzhen <czdswzz@126.com>
2019-05-16 10:55:37 +0800

    Add files via upload

---
+++ FastApiGatewayDb.Ui/Filter/ErrorAttribute.cs
@@ -0,0 +1,34 @@
+﻿using FastUntility.Core.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace FastApiGatewayDb.Ui.Filter
+{ /// <summary>
+  /// 出错处理过滤器
+  /// </summary>
+    public class ErrorAttribute : ExceptionFilterAttribute
+    {
+        #region 出错处理
+        /// <summary>
+        /// 出错处理
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+
+            filterContext.ExceptionHandled = true;
+
+            filterContext.Result = new ContentResult { Content = filterContext.Exception.StackTrace };
+
+            var ip = filterContext.HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+            if (string.IsNullOrEmpty(ip))
+                ip = filterContext.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            BaseLog.SaveLog(string.Format("ip:{0}，错误内容:{1}", ip, filterContext.Exception.StackTrace), "web_error");
+            return;
+        }
+        #endregion
+    }
+}
---
+++ FastApiGatewayDb.Ui/Filter/PowerAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using FastUntility.Core.Base;
+using System.IO.Compression;
+
+namespace FastApiGatewayDb.Ui.Filter
+{
+    /// <summary>
+    /// 权限过滤器
+    /// </summary>
+    public class PowerAttribute : ActionFilterAttribute
+    {
+        #region 请求过滤器
+        /// <summary>
+        /// 请求过滤器
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            var controller = filterContext.Controller as Controller;
+
+            #region model验证处理
+            if (!controller.ModelState.IsValid)
+            {
+                var item = controller.ViewData.ModelState.Values.ToList().Find(a => a.Errors.Count > 0);
+                var error = item.Errors.Where(a => !string.IsNullOrEmpty(a.ErrorMessage)).Take(1).SingleOrDefault().ErrorMessage;
+                var dic = new Dictionary<string, object>();
+                dic.Add("success", false);
+                dic.Add("msg", error);
+                filterContext.Result = new JsonResult(BaseJson.ModelToJson(dic));
+                return;
+            }
+            #endregion
+        }
+        #endregion
+
+        #region 结果过滤器
+        /// <summary>
+        /// 结果过滤器
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            //压缩
+            if (filterContext.HttpContext.Response.StatusCode == 200)
+            {
+                var acceptEncoding = filterContext.HttpContext.Request.Headers["Accept-Encoding"].ToString().ToLower();
+                if (!string.IsNullOrEmpty(acceptEncoding))
+                {
+                    if (filterContext.HttpContext.Response.Headers["Content-encoding"].ToArray().Length > 0)
+                        filterContext.HttpContext.Response.Headers.Remove("Content-encoding");
+
+                    if (acceptEncoding.Contains("gzip"))
+                        filterContext.HttpContext.Response.Body = new GZipStream(filterContext.HttpContext.Response.Body, CompressionMode.Compress);
+                    else if (acceptEncoding.Contains("deflate"))
+                        filterContext.HttpContext.Response.Body = new DeflateStream(filterContext.HttpContext.Response.Body, CompressionMode.Compress);
+                }
+            }
+        }
+        #endregion
+    }
+}
