7909ce22bb68031e3ddd8cbdb3e2e3fffcd5dcec
weizhonzhen <czdswzz@126.com>
2019-05-29 16:36:49 +0800

    Add files via upload

---
+++ FastApiGatewayDb.Ui/Models/DownUrlModel.cs
@@ -0,0 +1,66 @@
+﻿using FastApiGatewayDb.Ui.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastApiGatewayDb.Ui.Models
+{
+    public class DownUrlModel
+    {
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "键")]
+        public string Key { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "下游url")]
+        public string Url { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "下游名称")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "请求方法")]
+        [Method]
+        public string Method { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否流")]
+        public int IsBody { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "协议")]
+        [Protocol]
+        public string Protocol { get; set; }
+
+        [Display(Name = "soap方法")]
+        public string SoapMethod { get; set; }
+        
+        [Display(Name = "soap参数")]
+        public string SoapParamName { get; set; }
+        
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否解码")]
+        public int IsDecode { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否结果")]
+        public int IsResult { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(1, 2)]
+        [Display(Name = "参数来源")]
+        public int SourceParam { get; set; }
+        
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(1, 9)]
+        [Display(Name = "排序")]
+        public int OrderBy { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [StringLength(2)]
+        [Display(Name = "无响应等待下次请求时间")]
+        public string WaitHour { get; set; }
+    }
+}
---
+++ FastApiGatewayDb.Ui/Models/UrlModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FastApiGatewayDb.Ui.Validation;
+
+namespace FastApiGatewayDb.Ui.Models
+{
+    public class UrlModel
+    {
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "键")]
+        public string Key { get; set; }
+
+        [Schema(IsNull =true)]
+        [Display(Name = "请求类型")]
+        public string Schema { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0,1)]
+        [Display(Name = "是否缓存")]
+        public int IsCache { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 99)]
+        [Display(Name = "缓存时间天")]
+        public int CacheTimeOut { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否匿名访问")]
+        public int IsAnonymous { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否获取token接口")]
+        public int IsGetToken { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否数据日记")]
+        public int IsDbLog { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Range(0, 1)]
+        [Display(Name = "是否文本日记")]
+        public int IsTxtLog { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "名称")]
+        public string Name { get; set; }
+    }
+}
