a1638181f6b84d33c26fa1b0d5c6533d8f0ac1ec
weizhonzhen <czdswzz@126.com>
2019-05-30 22:12:44 +0800

    Add files via upload

---
+++ FastApiGatewayDb.Ui/Validation/UserAttribute.cs
@@ -0,0 +1,20 @@
+﻿using FastUntility.Core.Base;
+using FastApiGatewayDb.DataModel;
+using System.ComponentModel.DataAnnotations;
+using FastData.Core;
+
+namespace FastApiGatewayDb.Ui.Validation
+{
+    public class UserAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var count = FastRead.Query<ApiGatewayLogin>(a => a.UserName.ToLower() == value.ToStr().ToLower(), null, App.DbKey.Api).ToCount();
+
+            if (count > 0)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult(ErrorMessage = "用户名不正确");
+        }
+    }
+}
