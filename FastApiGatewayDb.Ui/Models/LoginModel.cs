570e75f1874e3d9238dab227400dc241fded03ac
weizhonzhen <czdswzz@126.com>
2019-05-30 22:12:12 +0800

    Add files via upload

---
+++ FastApiGatewayDb.Ui/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using FastApiGatewayDb.Ui.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastApiGatewayDb.Ui.Models
+{
+    public class LoginModel
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "用户名")]
+        [User]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required(ErrorMessage = "{0}不能为空")]
+        [Display(Name = "密码")]
+        public string Pwd { get; set; }
+    }
+}
