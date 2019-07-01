b87623ff8ebdeae31fc6f10922c7d546afea51fc
weizhonzhen <czdswzz@126.com>
2019-07-01 22:06:02 +0800

    Update UserController.cs

--- FastApiGatewayDb.Ui/Controllers/UserController.cs
+++ FastApiGatewayDb.Ui/Controllers/UserController.cs
@@ -1,14 +1,14 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FastApiGatewayDb.DataModel;
 using FastApiGatewayDb.Ui.Models;
 using FastData.Core;
 using FastData.Core.Context;
-using FastUntility.Core.Base;
 using FastUntility.Core.Page;
 using Microsoft.AspNetCore.Mvc;
 using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FastApiGatewayDb.Ui.Controllers
 {
@@ -50,20 +50,67 @@ namespace FastApiGatewayDb.Ui.Controllers
         }
         #endregion
 
+        #region 加载用户操作
+        /// <summary>
+        /// 加载用户操作
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Option(string key)
+        {
+            var list = new List<ApiGatewayUrl>();
+            var model = new UserModel();
+            model.AppKey = key;
+
+            using (var db = new DataContext(App.DbKey.Api))
+            {
+                list = FastRead.Query<ApiGatewayUrl>(a => a.Key != "", a => new { a.Key, a.Name }).ToList<ApiGatewayUrl>(db);
+
+                if (!string.IsNullOrEmpty(key))
+                {
+                    var info = FastRead.Query<ApiGatewayUser>(a => a.AppKey.ToLower() == key.ToLower()).ToItem<ApiGatewayUser>();
+                    model.Ip = info.Ip;
+                    model.Power = info.Power;
+                    if (!string.IsNullOrEmpty(model.Power))
+                        ViewBag.Power = model.Power.Split(",");
+                    else
+                        ViewBag.Power = new string[0];
+                }
+            }
+
+            ViewBag.List = list;
+            ViewData.Model = model;
+            return View();
+        }
+        #endregion
+
         #region 用户操作
         /// <summary>
         /// 用户操作
         /// </summary>
         /// <returns></returns>
-        public IActionResult Option(QueryPageUrlModel item)
+        [HttpPost]
+        public IActionResult UserOption(UserModel item)
         {
-            if (string.IsNullOrEmpty(item.Key))
-                return Content("key不能为空");
-            else
-            {
+            var isSuccess = false;
+            var model = new ApiGatewayUser();
 
-                return View();
+            model.AppKey = item.AppKey;
+            model.AppSecret = Guid.NewGuid().ToString().Substring(0, 31);
+            model.Power = item.Power;
+            model.Ip = item.Ip;
+
+            using (var db = new DataContext(App.DbKey.Api))
+            {
+                if (FastRead.Query<ApiGatewayUser>(a => a.AppKey == model.AppKey).ToCount(db) > 0)
+                    isSuccess = db.Update<ApiGatewayUser>(model, a => a.AppKey.ToLower() == model.AppKey.ToLower(), a => new { a.AppKey, a.Ip, a.Power }).writeReturn.IsSuccess;
+                else
+                    isSuccess = db.Add(model).writeReturn.IsSuccess;
             }
+
+            if (isSuccess)
+                return Json(new { success = isSuccess, msg = "操作成功" });
+            else
+                return Json(new { success = isSuccess, msg = "操作失败" });
         }
         #endregion
     }
