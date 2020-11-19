8bada01c2c7fa244298a21afe97a2273f1227a19
weizhonzhen <czdswzz@126.com>
2020-11-19 08:07:36 +0800

    Add files via upload

---
+++ FastApiGateway.Db/FastApiGatewayDbExtension.cs
@@ -0,0 +1,15 @@
+﻿using FastApiGatewayDb;
+using FastUntility.Core;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FastApiGatewayDbExtension
+    {
+        public static IServiceCollection AddFastApiGatewayDb(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IFastApiGatewayDb, FastApiGatewayDb.FastApiGatewayDb>();
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+            return serviceCollection;
+        }
+    }
+}
